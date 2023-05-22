2973252c1092358a63eb39b15c0cac8029d45c97
Wonderful <yunlong870924@gmail.com>
2023-05-22 19:12:36 +0900

    changed styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,58 +1,71 @@
-import React from 'react';
-import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImage, MDBBtn } from 'mdb-react-ui-kit';
+import React from "react";
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardImage,
+  MDBBtn,
+} from "mdb-react-ui-kit";
 import Web3 from "web3";
 import { useWeb3React } from "@web3-react/core";
 import Web3EthContract from "web3-eth-contract";
 
 const style = {
-    //backgroundColor: 'grey'
+  //backgroundColor: 'grey'
 };
 
 var nftOwner;
 let account;
 
 function trasferNFT(abi, address, tokenId) {
+  const tokenContract = new Web3EthContract(abi, address);
+  tokenContract.setProvider(
+    "https://speedy-nodes-nyc.moralis.io/036063875a28828fa0c00596/polygon/mumbai"
+  );
 
-    const tokenContract = new Web3EthContract(abi, address);
-    tokenContract.setProvider('https://speedy-nodes-nyc.moralis.io/036063875a28828fa0c00596/polygon/mumbai');
-
-    tokenContract.methods.transferOwnership(account.account).send({
-        from: account.account,
-    })
+  tokenContract.methods.transferOwnership(account.account).send({
+    from: account.account,
+  });
 }
 
 function getNFTOwner(abi, address) {
+  const tokenContract = new Web3EthContract(abi, address);
+  tokenContract.setProvider(
+    "https://speedy-nodes-nyc.moralis.io/036063875a28828fa0c00596/polygon/mumbai"
+  );
 
-    const tokenContract = new Web3EthContract(abi, address);
-    tokenContract.setProvider('https://speedy-nodes-nyc.moralis.io/036063875a28828fa0c00596/polygon/mumbai');
-
-    tokenContract.methods.owner().call().then(result => {
-        nftOwner = result;
+  tokenContract.methods
+    .owner()
+    .call()
+    .then((result) => {
+      nftOwner = result;
     });
-
 }
 
 export default function Card(props) {
-    getNFTOwner(props.contractAbi, props.contractAddress);
-    account = useWeb3React();
-    return (
-        <MDBCard style={{ maxWidth: '42rem', backgroundColor: '#222222' }}>
-            <MDBCardImage src={props.image} position='top' alt='...' />
-            <MDBCardBody>
-                <MDBCardTitle>{props.name}</MDBCardTitle>
-                <MDBCardText>
-                    {props.description}
-                </MDBCardText>
-                <hr />
-                <div style={style}>
-                    Owner: {nftOwner}
-                    <br />
-                </div>
-                <br />
-                <MDBBtn onClick={() => {
-                    trasferNFT(props.contractAbi, props.contractAddress, props.tokenID);
-                }}>Buy NFT</MDBBtn>
-            </MDBCardBody>
-        </MDBCard>
-    );
+  getNFTOwner(props.contractAbi, props.contractAddress);
+  account = useWeb3React();
+  return (
+    <MDBCard style={{ maxWidth: "42rem", backgroundColor: "#222222" }}>
+      <MDBCardImage src={props.image} position="top" alt="..." />
+      <MDBCardBody>
+        <MDBCardTitle>{props.name}</MDBCardTitle>
+        <MDBCardText>{props.description}</MDBCardText>
+        <hr />
+        <div style={style}>
+          Owner: {nftOwner}
+          <br />
+        </div>
+        <br />
+        <MDBBtn
+          onClick={() => {
+            trasferNFT(props.contractAbi, props.contractAddress, props.tokenID);
+          }}
+        >
+          Buy NFT
+        </MDBBtn>
+      </MDBCardBody>
+    </MDBCard>
+  );
 }
